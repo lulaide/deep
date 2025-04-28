f950c46718d89951152a6d4d4b0aa34dd84f6dad
Lulaide <lulaide85@gmail.com>
2025-04-28 19:33:26 +0800

    修改: 将py更变为notebook

--- main.py
+++
@@ -1,7 +0,0 @@
-import re
-with open("the-verdict.txt", "r") as file:
-    raw_text = file.read()
-
-preprocessed = re.split(r'([,.;:?_"()\']|--|\s)', raw_text)
-preprocessed = [item.strip() for item in preprocessed if item.strip()]
-print(preprocessed[:30])
